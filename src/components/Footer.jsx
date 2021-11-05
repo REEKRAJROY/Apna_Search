584dfbbb166d1503075fa696eafd0d41cb3a7e96
REEKRAJROY <reekrajroy@gmail.com>
2021-11-05 21:53:03 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    © 2021 Apna Search, Inc. Built with ❤️ by Reekraj Roy
+  </div>
+);
